c2b54f08e8a762ce1844733ba2576140452148ea
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2023-03-03 15:26:00 -0800

    initial commit

---
+++ src/main/resources/create.sql
@@ -0,0 +1,35 @@
+
+CREATE TABLE Caregivers (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Availabilities (
+    Time date,
+    Username varchar(255) REFERENCES Caregivers,
+    PRIMARY KEY (Time, Username)
+);
+
+CREATE TABLE Vaccines (
+    Name varchar(255),
+    Doses int,
+    PRIMARY KEY (Name)
+);
+
+
+CREATE TABLE Patients (
+    Username VARCHAR(255),
+    Salt BINARY(16),
+    Hash BINARY (16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointments (
+    appointment_id VARCHAR(50),
+    patient_username VARCHAR(255) REFERENCES Patients(Username),
+    caregiver_username VARCHAR(255) REFERENCES Caregivers(Username),
+    Time date,
+    vaccine_name VARCHAR(255) REFERENCES Vaccines(Name)
+);
